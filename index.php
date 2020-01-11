16a7984fa3cc45b6456cc95954dfa38cb49be68f
prakashsaran <om.axisweb@gmail.com>
2020-01-11 11:42:43 +0530

    update config

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 required_once("./src/QRgenearator.php");
-$imageConfig = [];
+$imageConfig = ['media_path'=>'./media/or',"secret_prefix"=>"TEST_PHP_SC"];
 $QrClass = new QRgenearator();
 echo $QrClass->genreartImage($imageConfig);
